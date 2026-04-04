e80319a9932f7553eaf0917ce98c264f41fb0d2c
Szymon Tondowski <stdeykun+git@gmail.com>
2026-04-04 20:22:38 +0200

    i18n added

---
+++ src/i18n.ts
@@ -0,0 +1,40 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import intervalPlural from "i18next-intervalplural-postprocessor";
+
+import { LOCAL_STORAGE } from "./constants";
+
+import { resources } from "./locales/config";
+
+export const SUPPORTED_LANGS = Object.keys(resources).sort((a, b) =>
+  a.localeCompare(b),
+);
+
+let defaultLanguage = "pl";
+
+if (SUPPORTED_LANGS.includes(document?.documentElement?.lang)) {
+  defaultLanguage = document?.documentElement?.lang;
+}
+
+const langFromLocalStorage =
+  localStorage.getItem(LOCAL_STORAGE.PICKED_LANGUAGE) || "";
+
+if (SUPPORTED_LANGS.includes(langFromLocalStorage)) {
+  defaultLanguage = langFromLocalStorage;
+}
+
+i18n
+  .use(initReactI18next)
+  .use(intervalPlural)
+  .init({
+    resources,
+    lng: defaultLanguage, // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+    // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
+    // if you're using a language detector, do not define the lng option
+    fallbackLng: "pl",
+    interpolation: {
+      escapeValue: false, // react already safes from xss
+    },
+  });
+
+export default i18n;
