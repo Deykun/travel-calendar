7141283252082ff6d5af0224625d4061fb901b89
Szymon Tondowski <stdeykun+git@gmail.com>
2026-05-01 09:44:08 +0200

    Account for year filter in determing future trips

--- src/features/calendar/components/calendar/Calendar.tsx
+++ src/features/calendar/components/calendar/Calendar.tsx
@@ -12,7 +12,7 @@ export const Calendar = ({ className }: Props) => {
       className={cn(
         "gap-4 @min-[1200px]:gap-8",
         "px-4 @min-[900px]:px-6 @min-[1200px]:px-12",
-        "grid grid-cols-1 @min-[780px]:grid-cols-2 @min-[1200px]:grid-cols-3 @min-[1500px]:grid-cols-4",
+        "grid grid-cols-1 @min-[780px]:grid-cols-2 @min-[1200px]:grid-cols-3 @min-[1600px]:grid-cols-4",
         className,
       )}
     >
