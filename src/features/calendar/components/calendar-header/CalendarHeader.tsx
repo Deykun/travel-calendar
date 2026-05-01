3da68b1ad9d4237ae2469c85bbba10ccb6b72308
Szymon Tondowski <stdeykun+git@gmail.com>
2026-05-01 12:23:38 +0200

    Clean up

--- src/features/calendar/components/calendar-header/CalendarHeader.tsx
+++ src/features/calendar/components/calendar-header/CalendarHeader.tsx
@@ -14,29 +14,23 @@ export function CalendarHeader() {
 
   return (
     <header className="mb-8">
-      {/* <h1 className="text-center text-white text-8xl font-semibold">
-        Travel Calendar
-      </h1> */}
       <div className={cn("grid text-center text-[180px] font-semibold")}>
-        {years.reverse().map((year, index) => (
+        {years.reverse().map((year) => (
           <span
             className={cn(
               "col-start-1 row-start-1 text-black",
-              // "duration-50",
               "transition-bounce",
               styles["year"],
               {
-                [`tedxt-[#d8da51] ${styles["year--active"]}`]: (
-                  activeFrom || ""
-                ).startsWith(String(year)),
+                [styles["year--active"]]: (activeFrom || "").startsWith(
+                  String(year),
+                ),
               },
             )}
             style={{
               opacity: Math.min(0.7, 2.25 / totalYears).toFixed(2),
-              // transform: `translateX(${index * 35}px) translateY(${index % 2 === 0 ? '-' : ''}40px)`,
             }}
           >
-            {/* '{String(year).slice(-2)} */}
             {year}
           </span>
         ))}
