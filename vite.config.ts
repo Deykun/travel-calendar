3d62cb82849b677ad451dfea09ff06582f556ebb
Szymon Tondowski <stdeykun+git@gmail.com>
2026-05-03 20:56:06 +0200

    🚀 Fixing the pathname for the build (#26)

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/travel-calendar/',
   plugins: [tailwindcss(), react(), babel({ presets: [reactCompilerPreset()] })],
   resolve: {
     alias: {
